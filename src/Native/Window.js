f941b17a8e09e7b6c38db8dd017c09567bbf0d14
Ram <quadrupleslap@gmail.com>
2017-04-29 23:44:41 +1000

    This is a lot more effort than it's worth.

--- src/Native/Window.js
+++ src/Native/Window.js
@@ -1,11 +1,9 @@
 var _angus_corp$taekwondo$Native_Window = (function() {
 
-function close() {
-  return _elm_lang$core$Native_Scheduler.nativeBinding(function (cb) {
-    window.close();
-    cb(_elm_lang$core$Native_Scheduler.fail(_elm_lang$core$Native_Utils.Tuple0));
-  });
-}
+var close = _elm_lang$core$Native_Scheduler.nativeBinding(function (cb) {
+  window.close();
+  cb(_elm_lang$core$Native_Scheduler.fail(_elm_lang$core$Native_Utils.Tuple0));
+});
 
 return {
   close: close
